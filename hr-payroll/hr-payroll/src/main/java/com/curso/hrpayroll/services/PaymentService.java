4aad06522440867ade986b34c6dac08cc22102bf
Renata Azevedo <renata.azevedo18@hotmail.com>
2021-06-07 17:08:29 -0300

    FeignClient

--- hr-payroll/hr-payroll/src/main/java/com/curso/hrpayroll/services/PaymentService.java
+++ hr-payroll/hr-payroll/src/main/java/com/curso/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.curso.hrpayroll.services;
 
 import com.curso.hrpayroll.entities.Payment;
 import com.curso.hrpayroll.entities.Worker;
+import com.curso.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,22 +14,23 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    @Autowired
-    private RestTemplate restTemplate;
+   @Autowired
+   private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerID, int days){
-    /*É necessário criar um mapa com os parametros que podem entrar na URL, nesse caso,
-    será um mapa com os IDs dos trabalhadores*/
+  /* AINDA NÃO É MICROSERVIÇOS
+    É necessário criar um mapa com os parametros que podem entrar na URL, nesse caso,
+    será um mapa com os IDs dos trabalhadores*//*
     Map<String, String> uriVariables = new HashMap<>();
     uriVariables.put("id", ""+workerID); //fazendo o mapa de parametros que vão entrar na URL
 
-    /*  Para utilizar o restTemplate é necessário fazer uma cópia da entidade que está na outra API*/
-    Worker worker = restTemplate.getForObject
-            (workerHost + "/workers/{id}", Worker.class, uriVariables); //eu passo a URL, a classe e os parametros que serão passados
-    return new Payment(worker.getName(),worker.getDailyInCome(),days);
+     /*  Para utilizar o restTemplate é necessário fazer uma cópia da entidade que está na outra API
+        Worker worker = restTemplate.getForObject
+                (workerHost + "/workers/{id}", Worker.class, uriVariables); //eu passo a URL, a classe e os parametros que serão passados*/
+
+        Worker worker = workerFeignClient.findById(workerID).getBody();
+
+        return new Payment(worker.getName(),worker.getDailyInCome(),days);
     }
 }
 
