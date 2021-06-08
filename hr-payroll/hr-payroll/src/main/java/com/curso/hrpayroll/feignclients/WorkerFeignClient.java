e8ba99106ec09f959941c8eea90cd91fba03459c
Renata Azevedo <renata.azevedo18@hotmail.com>
2021-06-08 12:14:29 -0300

    Configuring Zuul API Gateway

--- hr-payroll/hr-payroll/src/main/java/com/curso/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/hr-payroll/src/main/java/com/curso/hrpayroll/feignclients/WorkerFeignClient.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @Component
 @FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
